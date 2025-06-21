a53e49a4a3527a46b721317177f00cf82220b241
Sanskar Biyani <sanskarbiyani902@gmail.com>
2025-06-21 14:37:19 +0530

    Health Check path updated.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/healthCheck", tags=["health"])
 def health_check():
     """Health check endpoint."""
     return {"status": "ok"}
